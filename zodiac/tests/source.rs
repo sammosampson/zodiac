f1dd6b88d35ab226aebc7e400005bb7600ef6f53
Chris Sampson <csampson@appliedsystems.com>
2021-04-18 10:04:55 +0100

    started error tests

--- zodiac/tests/source.rs
+++ zodiac/tests/source.rs
@@ -200,7 +200,6 @@ fn nonexistent_imported_then_created_control_gets_output() {
     assert_eq!(changes[0], RenderPrimitive::circle([0, 0], 400, [0.4, 0.4, 0.4, 0.1], [1.0, 1.0, 1.0, 1.0], 3.0));
 }
 
-
 #[test]
 fn imported_then_deleted_then_recreated_control_gets_output() {
     let new_control = "
@@ -241,4 +240,79 @@ fn imported_then_deleted_then_recreated_control_gets_output() {
     
     assert_eq!(changes.len(), 1);
     assert_eq!(changes[0], RenderPrimitive::circle([0, 0], 400, [0.4, 0.4, 0.4, 0.1], [1.0, 1.0, 1.0, 1.0], 3.0));
+}
+
+#[test]
+fn invalid_source_causes_top_level_error_control_output() {
+    let root = "
+<root>
+    <circle
+        radius=x
+    />
+</root>
+";  
+    let mut world = World::default();
+    let mut resources = build_zodiac_resources();
+    let mut schedule = build_zodiac_systems_schedule();
+    
+    apply_initial_source(&mut resources, ".\\root.zod", root);
+    notify_resize_root_window(&mut world, (100, 100));
+    schedule.execute(&mut world, &mut resources);
+    
+    let changes: Vec::<RenderPrimitive> = <&RenderPrimitive>::query()
+        .iter(&mut world)
+        .map(|change| *change)
+        .collect();
+    
+    assert_eq!(changes.len(), 1);
+    assert_eq!(changes[0], RenderPrimitive::rectangle([0, 0], [100, 100], [1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], 0.0, [0, 0, 0, 0]));
+}
+
+#[test]
+fn invalid_import_causes_top_level_error_control_output() {
+    let root = "
+<import name=\"big-control\" path=\".\\big_control.zod\" />     
+<root>
+    <big-control/>
+</root>
+";  
+    let mut world = World::default();
+    let mut resources = build_zodiac_resources();
+    let mut schedule = build_zodiac_systems_schedule();
+    
+    apply_initial_source(&mut resources, ".\\root.zod", root);
+    notify_resize_root_window(&mut world, (100, 100));
+    schedule.execute(&mut world, &mut resources);
+        
+    let changes: Vec::<RenderPrimitive> = <&RenderPrimitive>::query()
+        .iter(&mut world)
+        .map(|change| *change)
+        .collect();
+    
+    assert_eq!(changes.len(), 1);
+    assert_eq!(changes[0], RenderPrimitive::rectangle([0, 0], [100, 100], [1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], 0.0, [0, 0, 0, 0]));
+}
+
+#[test]
+fn invalid_control_causes_top_level_error_control_output() {
+    let root = "
+<root>
+    <big-control/>
+</root>
+";  
+    let mut world = World::default();
+    let mut resources = build_zodiac_resources();
+    let mut schedule = build_zodiac_systems_schedule();
+    
+    apply_initial_source(&mut resources, ".\\root.zod", root);
+    notify_resize_root_window(&mut world, (100, 100));
+    schedule.execute(&mut world, &mut resources);
+        
+    let changes: Vec::<RenderPrimitive> = <&RenderPrimitive>::query()
+        .iter(&mut world)
+        .map(|change| *change)
+        .collect();
+    
+    assert_eq!(changes.len(), 1);
+    assert_eq!(changes[0], RenderPrimitive::rectangle([0, 0], [100, 100], [1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], 0.0, [0, 0, 0, 0]));
 }
