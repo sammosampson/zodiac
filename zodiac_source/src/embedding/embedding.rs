7c3c4ba9efc1a2b54e743febc16fd88204448541
Chris Sampson <csampson@appliedsystems.com>
2021-06-05 18:02:11 +0100

    Completed mox embedding

--- zodiac_source/src/embedding/embedding.rs
+++ zodiac_source/src/embedding/embedding.rs
@@ -4,17 +4,77 @@ use super::nodes::*;
 use crate::building::*;
 
 element! {
+    <canvas>
+    [LayoutContent::canvas()]
+    attributes {
+        left(u16)
+        top(u16)
+        width(u16)
+        height(u16)
+    }
+}
+
+element! {
     <vertical_stack>
     [LayoutContent::vertical()]
+    attributes {
+        left(u16)
+        top(u16)
+        width(u16)
+        height(u16)
+    }
 }
+
+element! {
+    <horizontal_stack>
+    [LayoutContent::horizontal()]
+    attributes {
+        left(u16)
+        top(u16)
+        width(u16)
+        height(u16)
+    }
+}
+
 
 element! {
     <circle>
     [Renderable::circle()]
     attributes {
+        left(u16)
+        top(u16)
         radius(u16)
-        stroke_width(u16)
+        colour(Colour)
         stroke_colour(StrokeColour)
+        stroke_width(u16)
+    }
+}
+
+element! {
+    <rect>
+    [Renderable::rectangle()]
+    attributes {
+        left(u16)
+        top(u16)
+        width(u16)
+        height(u16)
+        colour(Colour)
+        stroke_colour(StrokeColour)
+        stroke_width(u16)
+        corner_radii((u16, u16, u16, u16))
+    }
+}
+
+element! {
+    <text>
+    [Renderable::text()]
+    attributes {
+        left(u16)
+        top(u16)
+        width(u16)
+        height(u16)
+        colour(Colour)
         content(String)
+        font_size(u8)
     }
 }
